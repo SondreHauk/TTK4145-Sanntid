deb100bb852c4a90aff407de9f2aec5f1d108b6b
Sondre Hauk Heier Holte <sondreholte@Sondre-sin-MacBook-Pro.local>
2025-01-19 17:47:48 +0100

    localElevator added

---
+++ source/localElevator/elevator/elevator.go
@@ -0,0 +1,6 @@
+package elevator
+
+// This module should contain the elevator struct and some actions that the elveator can perform
+
+// What should the elevator struct contain (floor, direction, state, id, etc.)?
+// What actions should the elevator be able to perform (open door, close door, move, etc.)?
