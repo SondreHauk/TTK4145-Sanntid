f5168159db71e4feac872e2d707f32ede841370a
Lorang Strand <Lorangs@stud.ntnu.no>
2025-01-20 10:14:24 +0100

    lessgo

--- source/localElevator/elevator/elevator.go
+++ source/localElevator/elevator/elevator.go
@@ -1,6 +1,35 @@
 package elevator
 
-// This module should contain the elevator struct and some actions that the elveator can perform
+import (
+	"localElevator/elevio"
+	requests "localElevator/requestst"
+)
 
+// This module should contain the elevator struct and some actions that the elevator can perform
 // What should the elevator struct contain (floor, direction, state, id, etc.)?
 // What actions should the elevator be able to perform (open door, close door, move, etc.)?
+
+type elevator_unit struct {
+	floor     int
+	direction int
+	state     int
+	requests  [4][3]bool
+}
+
+func init_elevator(){
+
+}
+
+func lights(elev elevator_unit){
+	elevio.SetFloorIndicator(elev.floor)
+	for req := range elev.requests{
+		elevio.SetButtonLamp(elevio.BT_Cab,elev.floor,)
+}
+
+func door_open(){
+
+}
+
+func door_close(){
+	
+}
