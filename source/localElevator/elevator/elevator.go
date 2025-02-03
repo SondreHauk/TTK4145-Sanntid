2af755acd303fee5965f798640fe37328b2fc827
Sondre Hauk Heier Holte <sondreholte@Sondre-sin-MacBook-Pro.local>
2025-02-03 12:22:10 +0100

    hjbj

--- source/localElevator/elevator/elevator.go
+++ source/localElevator/elevator/elevator.go
@@ -12,51 +12,52 @@ import (
 
 type Elevator struct {
 	Floor     int
-	Direction Direction
-	Behavior  ElevatorBehaviour
+	Direction MotorDirection
+	State  ElevatorState
 	Requests  [NUM_FLOORS][NUM_BUTTONS]bool
+	//IsConnected bool  ??
 }
 
 //Drives down to the nearest floor and updates floor indicator
 func (elev *Elevator)ElevatorInit(){
-	for elevio.GetFloor() == -1{
+	for GetFloor() == -1{
 		time.Sleep(time.Millisecond*20)
-		elevio.SetMotorDirection(elevio.MD_Down)
+		SetMotorDirection(MD_Down)
 	}
-	elevio.SetMotorDirection(elevio.MD_Stop)
-	elev.Floor = elevio.GetFloor()
-	elevio.SetFloorIndicator(elev.Floor)
+	SetMotorDirection(MD_Stop)
+	elev.Floor = GetFloor()
+	SetFloorIndicator(elev.Floor)
 }
 
 
 //Moves to floor fl and updates floor indicators along the way.
 func (elev *Elevator)MoveFloor(fl int){
-	elev.Floor = elevio.GetFloor()
+	elev.Floor = GetFloor()
 	if elev.Floor == -1{
 		elev.ElevatorInit()
 	}
 	
 	if elev.Floor < fl{
-		elevio.SetMotorDirection(elevio.MD_Up)
+		SetMotorDirection(MD_Up)
 	}else if elev.Floor > fl{
-		elevio.SetMotorDirection(elevio.MD_Down)
+		SetMotorDirection(MD_Down)
 	}
 	
 	for elev.Floor != fl{
-		if elevio.GetFloor() != -1{
-			elevio.SetFloorIndicator(elev.Floor)
+		if GetFloor() != -1{
+			SetFloorIndicator(elev.Floor)
 		}
 		time.Sleep(time.Millisecond*20)
-		elev.Floor = elevio.GetFloor()
+		elev.Floor = GetFloor()
 	}
-	elevio.SetMotorDirection(elevio.MD_Stop)
-	elevio.SetFloorIndicator(elev.Floor)
+	SetMotorDirection(MD_Stop)
+	SetFloorIndicator(elev.Floor)
 }
 
 func (elev *Elevator)OpenDoor(){
-	elevio.SetDoorOpenLamp(true)
+	SetDoorOpenLamp(true)
 }
 
 func (elev *Elevator)CloseDoor(){
-	elevio.SetDoorOpenLamp(false)
+	SetDoorOpenLamp(false)
 }
