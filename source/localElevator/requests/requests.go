9365413b2a20a7efa5cc93c57e0c3f8f286801a3
sjur2k <sjur.groven@gmail.com>
2025-02-10 15:23:23 +0100

    Run2 is working!

--- source/localElevator/requests/requests.go
+++ source/localElevator/requests/requests.go
@@ -9,17 +9,17 @@ import (
 )
 
 //Clears Lights and Request Matrix
-func ClearFloor(elev Elevator, floor int) {
+func ClearFloor(elev *Elevator, floor int) {
 	/* elev.Requests[floor]= [NUM_BUTTONS]bool{} */
 	for btn := 0; btn < NUM_BUTTONS; btn++ {
 		elev.Requests[floor][btn] = false
 		elevio.SetButtonLamp(elevio.ButtonType(btn), floor, false)
 	}
 	//fmt.Printf("elev.Requests[%d] = %t\n",floor, elev.Requests[floor])
-	fmt.Printf("Floor %d cleared\n", floor)
+	fmt.Printf("Floor %d cleared\n", floor+1)
 }
 
-func ClearAll(elev Elevator) {
+func ClearAll(elev *Elevator) {
 	for fl := 0; fl < NUM_FLOORS; fl++ {
 		ClearFloor(elev, fl)
 	}
@@ -29,7 +29,7 @@ func Update(Receiver chan elevio.ButtonEvent, Transmitter chan Order) {
 	for{
 		select {
 			case btn := <-Receiver:
-				Transmitter<-Order{btn.Floor, int(btn.Button), false}
+				Transmitter<-Order{btn.Floor, int(btn.Button)}//, false}
 				elevio.SetButtonLamp(elevio.ButtonType(btn.Button), btn.Floor, true) //THIS SIGNIFIES ORDER IS ACCEPTED. CHANGE
 		}
 		time.Sleep(20*time.Millisecond)
