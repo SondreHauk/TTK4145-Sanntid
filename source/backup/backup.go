c180bc5f22e8aa307639e88557bb0a006cb41a76
Sondre <shholte@stud.ntnu.no>
2025-02-24 14:55:47 +0100

    final commit before merging with main

--- source/backup/backup.go
+++ source/backup/backup.go
@@ -6,6 +6,11 @@ import (
 	"time"
 )
 
+/*
+The backup must keep a copy of the primary. It must hold elevators, activepeers, orders etc.
+The primary must therefore send this over to backup as a timed heartbeat and event driven.
+Maybe we can create one big struct containing everything, that can be sent from prim to back.
+*/
 
 func Run(fromprimary <-chan string, becomePrimary chan <- bool){
 	fmt.Println("Enter Backup mode - listening for primary")
