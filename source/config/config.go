e64f80c76910df6e5940a603a7550860ab552d08
sjur2k <sjur.groven@gmail.com>
2025-03-05 16:15:21 +0100

    littrydding

--- source/config/config.go
+++ source/config/config.go
@@ -4,64 +4,86 @@ import (
 	"time"
 )
 
-type ElevatorState int
-
 const (
-    IDLE ElevatorState = iota
+	IDLE ElevatorState = iota
 	MOVING
 	DOOR_OPEN
 	EMERGENCY_AT_FLOOR
 	EMERGENCY_IN_SHAFT
 )
 
-const(
-	NUM_FLOORS = 4
-	NUM_BUTTONS = 3
+const (
+	NUM_FLOORS    = 4
+	NUM_BUTTONS   = 3
 	NUM_ELEVATORS = 1 // FOR NOW
 )
 
 const (
-	T_HEARTBEAT = time.Millisecond*500 //Must be much faster than .5 s
-	T_SLEEP = time.Millisecond*20
-	T_DOOR_OPEN = time.Second*3
-	T_TRAVEL = time.Second*2 	//Approximate time to travel from floor i to floor i+-1
-	T_TIMEOUT = time.Second*1
-	T_BLINK = time.Millisecond*100
+	T_HEARTBEAT = time.Millisecond * 500 //Must be much faster than .5 s
+	T_SLEEP     = time.Millisecond * 20
+	T_DOOR_OPEN = time.Second * 3
+	T_TRAVEL    = time.Second * 2 //Approximate time to travel from floor i to floor i+-1
+	T_TIMEOUT   = time.Second * 1
+	T_BLINK     = time.Millisecond * 100
 )
 
-const(
-	UP = 1
+const (
+	UP   = 1
 	DOWN = -1
 	STOP = 0
 )
 
-//Is possible to use only one port, with msg IDs.
-const(
-	PORT_PEERS = 20020
-	PORT_ELEVSTATE = 20030
-	PORT_WORLDVIEW = 20040
-	PORT_REQUEST = 20050
-	PORT_ORDER = 20060
+// TODO: Only two ports necessary
+const (
+	PORT_PEERS      = 20020
+	PORT_ELEVSTATE  = 20030
+	PORT_WORLDVIEW  = 20040
+	PORT_REQUEST    = 20050
+	PORT_ORDER      = 20060
 	PORT_HALLLIGHTS = 20070
 )
 
+type ElevatorState int
+
 type Elevator struct {
-	Id string
-	Floor     int
-	Direction int
+	Id            string
+	Floor         int
+	Direction     int
 	PrevDirection int
-	State  ElevatorState
-	Orders  [NUM_FLOORS][NUM_BUTTONS]bool
+	State         ElevatorState
+	Orders        [NUM_FLOORS][NUM_BUTTONS]bool
 }
 
 type Order struct {
-	Id string
-	Floor int
+	Id     string
+	Floor  int
 	Button int
 }
+type PeerUpdate struct {
+	Peers []string
+	New   string
+	Lost  []string
+}
 
-const(
-	HALLUP = iota
-	HALLDWN
-	CAB
-)
+
+//----------------PRIMARY/BACKUP--------------------
+
+type Worldview struct {
+	PrimaryId     string
+	PeerInfo      PeerUpdate
+	FleetSnapshot map[string]Elevator // Owned by
+}
+
+func WorldviewConstructor(PrimaryId string, PeerInfo PeerUpdate, FleetSnapshot map[string]Elevator) Worldview {
+	return Worldview{PrimaryId: PrimaryId, PeerInfo: PeerInfo, FleetSnapshot: FleetSnapshot}
+}
+
+type FleetAccess struct {
+	Cmd     string //{"read","write one","write all"}
+	Id      string
+	Elev    Elevator
+	ElevMap map[string]Elevator
+	ReadCh  chan map[string]Elevator
+}
+
+//--------------------------------------------
